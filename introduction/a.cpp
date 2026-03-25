2341c06c4dd4ac9e1655ac4d4702764989403814
Andhika Rahman <andhikarahmanp86@gmail.com>
2026-03-25 13:51:32 +0700

    add some comments

--- introduction/a.cpp
+++ introduction/a.cpp
@@ -1,8 +1,17 @@
+/* 
+  Chapter: Perkenalan Pemrograman Kompetitif
+  Problem: Perkenalan Soal Implementasi (Matrix Rotation)
+  Time limit: 1 s | Memory limit: 64 MB
+  C++20 | GCC 15.2.0
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Define an alias for vector<int>
 typedef vector<int> vi;
 
+// Function to perform a transpose operation on a matrix
 vector<vi> transpose(const vector<vi>& mat) {
     int r = mat.size();
     int c = mat[0].size();
@@ -17,6 +26,7 @@ vector<vi> transpose(const vector<vi>& mat) {
     return res;
 }
 
+// Function to rotate a matrix 90 degrees clockwise
 vector<vi> rotate(const vector<vi>& mat) {
     int r = mat.size();
     int c = mat[0].size();
@@ -33,19 +43,27 @@ vector<vi> rotate(const vector<vi>& mat) {
     return res;
 }
 
+// Main function
 int main() {
+
+    // Declaring variables
     int r, c;
 
+    // Input
     cin >> r >> c;
 
+    // Declaring a matrix
     vector<vi> mat(r, vi(c));
 
+    // Enter matrix elements
     for(int i = 0; i < r; i++)
         for(int j = 0; j < c; j++)
             cin >> mat[i][j];
 
+    // Rotate the matrix
     vector<vi> rotated = rotate(mat);
 
+    // Print the final matrix condition
     for(int i = 0; i < c; i++) {
         for(int j = 0; j < r; j++)
             cout << rotated[i][j] << " ";
